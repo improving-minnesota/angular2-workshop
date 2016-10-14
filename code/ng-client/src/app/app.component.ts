8ca69b3647609de7038ef6abf574791323da6a39
Jon DeJong <jon.dejong@gmail.com>
2016-10-14 14:48:38 -0500

    Lab 1 Start

--- code/ng-client/src/app/app.component.ts
+++ code/ng-client/src/app/app.component.ts
@@ -1,23 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 
-import {LocalStorage, AUTH_TOKEN_NAME} from './auth/localStorage';
-import {LoginService} from './shared/login.service';
-
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(private storage: LocalStorage, private loginService: LoginService) {}
+  constructor() {}
+
+  message:String;
 
   ngOnInit() {
-    this.storage.initStorage(window.localStorage);
-
-    const token = this.storage.getItem(AUTH_TOKEN_NAME);
-    if (token) {
-      this.loginService.loadUser(token);
-    }
+    this.message = 'Hello and welcome to the workshop!'
   }
 
 }
